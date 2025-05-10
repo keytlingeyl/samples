7c6ea0f23f110ad27c18d71a223ed16a0c341380
Caitlin Gail Abizar <162838688+keytlingeyl@users.noreply.github.com>
2025-05-10 16:06:40 +0800

    Add files via upload

---
+++ Semantic/main.js
@@ -0,0 +1,9 @@
+
+function home() {
+  document.getElementById("about").style.display = "none";
+}
+
+function about() {
+  document.getElementById("about").style.display = "block";
+  document.getElementById("home").style.display = "none";
+}
